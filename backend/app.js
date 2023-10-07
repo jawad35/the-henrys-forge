58f1f298c870d7921ce316ab94d2ee451da732e2
jawad35 <jaqimughal@gmail.com>
2023-10-07 04:21:14 -0700

    New Directory

--- backend/app.js
+++ backend/app.js
@@ -41,10 +41,10 @@ app.get('/api/config/paypal', (req,res)=>{
 })
 
 
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 // Middleware for Errors
